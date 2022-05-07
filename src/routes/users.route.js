f023668f6c3d4d7793035174cc1aa2b26065abb9
Alexandre <asilvestre786@gmail.com>
2022-05-07 16:58:57 -0300

    Proj4 - Auth Middleware

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const usersController = require("../controllers/users.controller");
 
 router.post("/create", usersController.createUserController);
-router.get("/login", usersController.findAllUserController);
+router.get("/", usersController.findAllUserController);
 
 module.exports = router;
 
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,10 +1,13 @@
 const User = require("../models/User");
-
+const findByEmailUserService = (email) => User.findOne ({email: email})
 const createUserService = (body) => User.create(body);
 
 const findAllUserService = () => User.find();
+const findByIdUserService = (idUser) => User.findById (idUser); 
 
 module.exports = {
   createUserService,
   findAllUserService,
+  findByEmailUserService,
+  findByIdUserService,
 };
